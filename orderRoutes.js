2457e59585a27239406865d8392f1fe241ad6108
salini-360 <salinisoundaiyan@gmail.com>
2026-06-23 15:03:58 +0530

    Update orderRoutes.js

--- orderRoutes.js
+++ orderRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Order = require("../models/Order");
+const Order = require("./Order");
 const twilio = require("twilio");
 
 const client = twilio(
